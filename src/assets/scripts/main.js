6db030a36a910ff16f878dfb6250003ed7692f40
slaughtbear <ivanaguirre368@gmail.com>
2026-03-05 17:36:38 -0600

    mi primer commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,17 @@
+
+
+
+if ('serviceWorker' in navigator) {
+    console.log('ServiceWorker es compatible');
+
+    window.addEventListener('load', function() {
+        this.navigator.serviceWorker.register('./sw.js')
+        .then(reg => console.log(`ServiceWorker registrado con éxito: (Scope: ${reg.scope}`))
+        .catch(err => console.log(`Fallo en el registro del ServiceWorker: ${err}`));
+    });
+} else {
+    console.log('ServiceWorker no es compatible');
+}
+
+
+ 
---
+++ src/sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = "v1_cache_mi_pdwa";
+
+const urlsToCache = [
+  "./",
+  "./index.php",
+  "./manifest.json",
+  "./assets/scripts/main.js",
+  "./assets/icons/icono.png",
+  "./assets/icons/icono-192.png",
+  "./assets/icons/icono-512.png",
+  "./assets/images/banner_gpa.jpg",
+  "./assets/images/perfil.jpg",
+  "./templates/banner.html",
+  "./templates/cta.html",
+  "./templates/experiencia.html",
+  "./templates/footer.html",
+  "./templates/formulario.html",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache).then(() => {
+          self.skipWaiting();
+        });
+      })
+      .catch((err) => console.log(`No se ha registrado el cache ${err}`)),
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          }),
+        );
+      })
+
+      .then(() => {
+        self.clients.claim();
+      }),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(e.request);
+    })
+  );
+});
